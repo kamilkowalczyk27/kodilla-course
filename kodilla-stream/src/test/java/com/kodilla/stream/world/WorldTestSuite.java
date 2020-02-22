b510c947b578ee6949827bc107c9fb319a50de65
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-02-22 09:42:09 +0100

    Adding Module 7.4

--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -1,28 +1,41 @@
 package com.kodilla.stream.world;
-
+import org.junit.Assert;
 import org.junit.Test;
+import java.math.BigDecimal;
+
 
 public class WorldTestSuite {
 
     @Test
-    public void testGetPeopleQuantity(){}
+    public void testGetPeopleQuantity() {
         //Given
-    World world = new World();
+        World world = new World();
+        BigDecimal polishPeople = new BigDecimal("1111111111");
+        BigDecimal japanesePeople = new BigDecimal("2222222222222");
+        BigDecimal sudanesePeople = new BigDecimal("333333333333333");
 
-    Continent europe = new Continent("Europe");
-    Continent asia = new Continent("Asia");
-    Continent africa = new Continent("Africa");
+        Continent europe = new Continent("Europe");
+        Continent asia = new Continent("Asia");
+        Continent africa = new Continent("Africa");
 
-    Country poland = new Country("Poland", ("222222222222222222"));
-    Country japan = new Country("Japan");
-    Country sudan = new Country("Sudan");
+        Country poland = new Country("Poland", polishPeople);
+        Country japan = new Country("Japan", japanesePeople);
+        Country sudan = new Country("Sudan", sudanesePeople);
 
-    world.addContinet(europe);
-    world.addContinent(asia);
-    world.addContinent(africa);
+        world.addContinent(europe);
+        world.addContinent(asia);
+        world.addContinent(africa);
 
-    europe.addCountry(poland);
-    asia.addCountry(japan);
-    africa.addCountry(sudan);
+        europe.addCountry(poland);
+        asia.addCountry(japan);
+        africa.addCountry(sudan);
 
+        //When
+        BigDecimal allPeople = world.getPeopleQuantity();
+
+        //Then
+        BigDecimal allPeopleExpected = new BigDecimal("335556666666666");
+        Assert.assertEquals(allPeople, allPeopleExpected);
+    }
 }
+
