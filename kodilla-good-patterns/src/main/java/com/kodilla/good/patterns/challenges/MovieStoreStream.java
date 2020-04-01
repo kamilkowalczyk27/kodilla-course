b98a28c8caca68dbd40624527af637712f3b72ff
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-04-01 17:35:22 +0200

    Adding Module 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreStream.java
@@ -0,0 +1,14 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStoreStream {
+
+    public static void main(String[] args) {
+        String moviesNames = MovieStore.getMovies().entrySet().stream()
+                .flatMap(s -> s.getValue().stream())
+                .collect(Collectors.joining("! "));
+
+        System.out.println(moviesNames);
+    }
+}
