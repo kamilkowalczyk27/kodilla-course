4e61c313b34cb8ade720eaeecdb5e9f2bb4a0d51
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-04-08 21:07:44 +0200

    Adding Module 14.1

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
-    String userName;
+    private String userName;
 
     public ForumUser() {
         userName = "John Smith";
