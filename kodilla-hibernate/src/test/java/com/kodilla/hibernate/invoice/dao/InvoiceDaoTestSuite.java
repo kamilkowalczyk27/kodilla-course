09b3f22ec33d0733719095a6e67a8019f10c421a
kamilkowalczyk27 <kamilkowalczyk27@o2.pl>
2020-06-16 23:02:01 +0200

    Adding

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -9,9 +9,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 
+@Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InvoiceDaoTestSuite {
@@ -49,8 +50,8 @@ public class InvoiceDaoTestSuite {
         Assert.assertNotEquals(0, idItem);
 
         //CleanUp
-        productDao.deleteAll();
-        itemDao.deleteAll();
-        invoiceDao.deleteAll();
+//        productDao.deleteAll();
+//        itemDao.deleteAll();
+//        invoiceDao.deleteAll();
     }
 }
